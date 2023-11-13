d9b6b9045b52ae80c8e20f729fa0a39fd5eb96cf
PChabowski <p.chabowski1@gmail.com>
2023-11-13 18:38:17 +0100

    upgrade theme

--- functions.php
+++ functions.php
@@ -1,20 +1,19 @@
 <?php
-wp_enqueue_style( 'style', get_stylesheet_uri() );
 
-if ( ! function_exists( 'myfirsttheme_setup' ) ) :
+if ( ! function_exists( 'pctheme_setup' ) ) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
 	 * Note that this function is hooked into the after_setup_theme hook, which runs
 	 * before the init hook.
 	 */
-	function myfirsttheme_setup() {
+	function pctheme_setup() {
 		// Add support for block styles.
 		add_theme_support( 'wp-block-styles' );
 	
 		// Enqueue editor styles.
 		add_editor_style( 'style.css' );
 	}
-	endif; // myfirsttheme_setup
-	add_action( 'after_setup_theme', 'myfirsttheme_setup' );
+	endif; // pctheme_setup
+	add_action( 'after_setup_theme', 'pctheme_setup' );
 	
